d5168cde33a2e8fd28d45d3d1bfcd7bc99a16695
Jorge Reus <jorge.gomez1@smiledirectclub.com>
2020-09-22 18:30:42 -0500

    First commit

--- consul/dashboard/entrypoint.sh
+++ consul/dashboard/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-consul connect envoy -sidecar-for 127.0.0.1:dashboard-service:9002 &
+CONSUL_IP=$(host consul_server | grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
+printf "options ndots:0\nnameserver $CONSUL_IP\nnameserver 127.0.0.11" > /etc/resolv.conf
+#consul connect envoy -sidecar-for 127.0.0.1:dashboard-service:9002 &
 dashboard-service
